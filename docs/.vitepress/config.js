0729b6e6a3d6f113bf3e38a6ddd070a3afce9126
HUBINET Brice <brice.hubinet@emundus.fr>
2024-07-22 15:05:43 +0200

    Update description

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "TipTapEditor",
-  description: "A VitePress Site",
+  description: "An editor for Vue.js based on TipTap package",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
